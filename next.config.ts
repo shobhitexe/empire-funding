120cf50a6fcf94eb3d0e198c3635dacc1a58cb6f
shobhitexe <sguptamds94@gmail.com>
2025-05-12 11:28:34 +0530

    Favicon add

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: { remotePatterns: [{ hostname: "www.facebook.com" }] },
 };
 
 export default nextConfig;
