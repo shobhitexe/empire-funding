e06c39c47413206ef931ba4710f254d664a1b551
shobhitexe <sguptamds94@gmail.com>
2025-05-12 11:24:37 +0530

    Pixel add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Figtree } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components";
 import { Toaster } from "@/components/ui/sonner";
+import Script from "next/script";
+import Image from "next/image";
 
 const figtree = Figtree({ variable: "--font-figtree", subsets: ["latin"] });
 
@@ -23,6 +25,33 @@ export default function RootLayout({
         {children}
         {/* <Footer /> */}
         <Toaster />
+
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '23936770399281696');
+          fbq('track', 'PageView');`,
+          }}
+        ></Script>
+
+        <noscript>
+          <Image
+            height="1"
+            width="1"
+            alt="fb"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=23936770399281696&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   );
