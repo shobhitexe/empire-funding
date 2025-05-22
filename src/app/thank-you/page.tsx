56109a004c8e211eb69fdecdf2511c37cc692fb3
shobhitexe <sguptamds94@gmail.com>
2025-05-22 20:32:46 +0530

    Add thank you page with purchase confirmation and tick image

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,31 @@
+import { buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function page() {
+  return (
+    <div className="sm:mt-20 mt-10 px-5">
+      <div
+        className="flex max-w-sm w-full mx-auto p-10 rounded-xl flex-col items-center justify-center gap-4"
+        style={{
+          background:
+            "linear-gradient(47.01deg, rgba(16, 185, 129, 0.16) 0%, rgba(52, 211, 153, 0.08) 50%, rgba(5, 150, 105, 0.16) 100%)",
+        }}
+      >
+        <Image src={"/images/tick.svg"} alt={"tick"} width={183} height={183} />
+
+        <div className="flex flex-col items-start justify-center gap-4">
+          <div className="font-bold text-2xl">Thank you</div>
+          <div>Your Purchase has been completed with $199.99.</div>
+          <div className="text-sm text-muted-foreground">
+            You will receive an email shortly. Please check the spam folder in
+            case.
+          </div>
+          <Link href={"/"} className={`${buttonVariants({})}`}>
+            Return to Dashboard
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
